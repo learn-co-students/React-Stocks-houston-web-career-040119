3940868243983679a086db3daa9e3723fedceb10
shawn0527 <shawnren527@gmail.com>
2019-06-08 15:12:24 -0500

    Done.

--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -4,28 +4,120 @@ import PortfolioContainer from './PortfolioContainer'
 import SearchBar from '../components/SearchBar'
 
 class MainContainer extends Component {
+  constructor() {
+    super()
+    this.state=({
+      stocks: [],
+      boughtStocks: [],
+      displayStocks: [],
+      displayBoughtStocks: []
+    })
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:3000/stocks')
+    .then(res => res.json())
+    .then(data => {
+      this.setState({
+        stocks: data,
+        displayStocks: data
+      })
+    })
+  }
+
+  tradeStock = (e) => {
+    let stock = this.state.stocks.find(stock => stock.id == e.target.id)
+    if(e.target.parentElement.parentElement.firstElementChild.innerText === 'Stocks') {
+      if(!this.state.boughtStocks.includes(stock)) {
+        this.state.boughtStocks.push(stock)
+        this.setState({
+          boughtStocks:this.state.boughtStocks,
+          displayBoughtStocks: this.state.boughtStocks
+        }) 
+      }
+    } else {
+      this.state.boughtStocks.pop(stock)
+      this.setState({
+        boughtStocks:this.state.boughtStocks,
+        displayBoughtStocks: this.state.boughtStocks
+      })
+    }
+  }
+
+  sortStocks = (e) => {
+    if(e.target.value == 'Price' && e.target.checked) {
+      let newArrA = this.state.displayStocks.sort((a,b) => a.price - b.price)
+      let newArrB = this.state.displayBoughtStocks.sort((a,b) => a.price - b.price)
+      this.setState({
+        displayStocks: newArrA,
+        displayBoughtStocks: newArrB
+      })
+    } 
+
+    if(e.target.value === 'Alphabetically' && e.target.checked) {
+      let newArrA = this.state.displayStocks.sort((a,b) => a.ticker < b.ticker?-1:1)
+      let newArrB = this.state.displayBoughtStocks.sort((a,b) => a.ticker < b.ticker?-1:1)
+      this.setState({
+        displayStocks: newArrA,
+        displayBoughtStocks: newArrB
+      })
+    }
+
+    if(e.target.value === 'Sportswear') {
+      let newArrA = this.state.stocks.filter(stock => stock.type === e.target.value)
+      let newArrB = this.state.boughtStocks.filter(stock => stock.type === e.target.value)
+      this.setState({
+        displayStocks: newArrA,
+        displayBoughtStocks: newArrB
+      })
+    }
+
+    if(e.target.value === 'Tech') {
+      let newArrA = this.state.stocks.filter(stock => stock.type === e.target.value)
+      let newArrB = this.state.boughtStocks.filter(stock => stock.type === e.target.value)
+      this.setState({
+        displayStocks: newArrA,
+        displayBoughtStocks: newArrB
+      })
+    }
+
+    if(e.target.value === 'Finance') {
+      let newArrA = this.state.stocks.filter(stock => stock.type === e.target.value)
+      let newArrB = this.state.boughtStocks.filter(stock => stock.type === e.target.value)
+      this.setState({
+        displayStocks: newArrA,
+        displayBoughtStocks: newArrB
+      })
+    }
+
+    if(e.target.value === 'All') {
+      this.setState({
+        displayStocks: this.state.stocks,
+        displayBoughtStocks: this.state.boughtStocks
+      })
+    }
+  }
 
   render() {
     return (
       <div>
-        <SearchBar/>
+        <SearchBar sortStocks={this.sortStocks}/>
 
           <div className="row">
             <div className="col-8">
 
-              <StockContainer/>
+              <StockContainer displayStocks={this.state.displayStocks} tradeStock={this.tradeStock}/>
 
             </div>
             <div className="col-4">
 
-              <PortfolioContainer/>
+              <PortfolioContainer tradeStock={this.tradeStock} displayBoughtStocks={this.state.displayBoughtStocks}/>
 
             </div>
           </div>
       </div>
     );
   }
-
 }
 
 export default MainContainer;
--- src/containers/PortfolioContainer.js
+++ src/containers/PortfolioContainer.js
@@ -2,12 +2,15 @@ import React, { Component } from 'react';
 import Stock from '../components/Stock'
 
 class PortfolioContainer extends Component {
+  constructor() {
+    super()
+  }
 
   render() {
     return (
       <div>
         <h2>My Portfolio</h2>
-          {
+          {this.props.displayBoughtStocks.map(stock => <Stock stock={stock} tradeStock={this.props.tradeStock}/>)
             //render your portfolio stocks here
           }
       </div>
